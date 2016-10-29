4f2a865c298493d9d19fcdc41d5ca0a4e1636600
Nestor Arocha <nesaro@gmail.com>
2016-10-29 20:19:36 +0100

    removes moved files

--- mp2/obtenciondatos.h
+++
@@ -1,2 +0,0 @@
-void obtencionDatos1(int tam[], int n, int nc_inv[], int nm_inv[], float t_inv[], float nc_ale[], float nm_ale[], float t_ale[]);
-void obtencionDatos2(int tam[], int n, int nc_inv[], int nm_inv[], float t_inv[], float nc_ale[], float nm_ale[], float t_ale[]);
