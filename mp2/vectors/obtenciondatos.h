6bb29d86a21c63e31fa95de4da818f75fc115052
Nestor Arocha <nesaro@gmail.com>
2016-10-29 20:18:34 +0100

    moves files to its own folder

---
+++ mp2/vectors/obtenciondatos.h
@@ -0,0 +1,2 @@
+void obtencionDatos1(int tam[], int n, int nc_inv[], int nm_inv[], float t_inv[], float nc_ale[], float nm_ale[], float t_ale[]);
+void obtencionDatos2(int tam[], int n, int nc_inv[], int nm_inv[], float t_inv[], float nc_ale[], float nm_ale[], float t_ale[]);
